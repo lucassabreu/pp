3b3f74a5358569bb198b7603ad4a0c02610486ce
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2020-11-10 08:36:28 -0300

    wip

---
+++ functions/src/entity/rss.ts
@@ -0,0 +1,16 @@
+// class Item {
+//   guid: string;
+//   title: string;
+//   link: string;
+//   description: string;
+// }
+
+// class RSS {
+//   title: string;
+//   description: string;
+//   link: string;
+//   image: string;
+//   items: Array<Item>;
+// }
+
+// export default RSS;

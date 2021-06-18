819886742897b1737bd75a12c83c8a544ec1dc7f
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2021-06-18 17:48:43 -0300

    (setup): firestore

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { functions } from "./service/firebase";
+import { db, functions } from "./service/firebase";
 
 function App() {
   const [result, setResult] = useState<string>("loading...");
@@ -12,6 +12,17 @@ function App() {
       .catch((err) => setResult(JSON.stringify(err)));
   }, [setResult]);
 
+  const [items, setItems] = useState<string[]>([]);
+  useEffect(
+    () =>
+      db
+        .collection("items")
+        .onSnapshot((snapshot) =>
+          setItems(snapshot.docs.map((i) => JSON.stringify(i.data())))
+        ),
+    [setItems]
+  );
+
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +31,10 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <p>Result: {result}</p>
+        <p>Items:</p>
+        {items.map((item, index) => (
+          <code key={index}>{item}</code>
+        ))}
         <a
           className="App-link"
           href="https://reactjs.org"
